2a78a4bbbf81efbc20e9e05d8a08c02b19594790
Sumit-63030 <sumit.dharmadhikari05@gmail.com>
2026-07-16 09:06:14 +0530

    integrate cloudinary resume upload

--- server/controllers/resumeController.js
+++ server/controllers/resumeController.js
@@ -1,12 +1,35 @@
-export const uploadResume = async(req , res) => {
-console.log("User:");
-  console.log(req.user);
+import cloudinary from "../lib/cloudinary.js";
+import streamifier from "streamifier";
+export const uploadResume = async (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({
+      message: "Please upload a PDF",
+    })
+  }
 
-  console.log("File:");
-  console.log(req.file);
+  const uploadStream = cloudinary.uploader.upload_stream(
+    {
+      resource_type: "raw",
+      folder: "resume-analyzer",
+    },
+    (error, result) => {
+      if(error)
+      {
+        console.log(error);
 
-  return res.status(200).json({
-    message: "Resume received successfully",
-  });
+        return res.status(500).json({
+          message : "Failed to upload resume",
+        })
+      }
+
+      return res.status(200).json({
+        message : "Resume uploaded successfully",
+        url : result.secure_url,
+      });
+
+    }
+  );
+
+  streamifier.createReadStream(req.file.buffer).pipe(uploadStream);
 
 };
