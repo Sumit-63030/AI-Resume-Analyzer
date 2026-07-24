6b73c2605987a3d7ed317fee5c465145c91836b6
Sumit-63030 <sumit.dharmadhikari05@gmail.com>
2026-07-24 12:01:52 +0530

    create login page UI

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,10 +1,49 @@
+import "../styles/Login.css";
+import { Link } from "react-router-dom";
 
-const Login = () => {
+function Login() {
   return (
-    <div>
-      Login
+    <div className="login">
+      <div className="login__card">
+
+        <h1 className="login__title">
+          AI Resume Analyzer
+        </h1>
+
+        <p className="login__subtitle">
+          Login to continue
+        </p>
+
+        <form className="login__form">
+
+          <input
+            className="login__input"
+            type="email"
+            placeholder="Email"
+          />
+
+          <input
+            className="login__input"
+            type="password"
+            placeholder="Password"
+          />
+
+          <button className="login__button">
+            Login
+          </button>
+
+        </form>
+
+        <p className="login__footer">
+          Don't have an account?
+          <Link className="login__link" to="/register">
+            Register
+          </Link>
+        </p>
+
+      </div>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
