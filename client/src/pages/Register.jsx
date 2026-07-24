1acbbedd16ddd66ad5c54f85a092a2576fd27437
Sumit-63030 <sumit.dharmadhikari05@gmail.com>
2026-07-24 12:07:31 +0530

    feat: create authentication pages UI

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,10 +1,55 @@
+import { Link } from "react-router-dom";
+import "../styles/Register.css";
 
-const Register = () => {
+function Register() {
   return (
-    <div>
-      register
+    <div className="register">
+      <div className="register__card">
+
+        <h1 className="register__title">
+          AI Resume Analyzer
+        </h1>
+
+        <p className="register__subtitle">
+          Create your account
+        </p>
+
+        <form className="register__form">
+
+          <input
+            className="register__input"
+            type="text"
+            placeholder="Full Name"
+          />
+
+          <input
+            className="register__input"
+            type="email"
+            placeholder="Email"
+          />
+
+          <input
+            className="register__input"
+            type="password"
+            placeholder="Password"
+          />
+
+          <button className="register__button">
+            Register
+          </button>
+
+        </form>
+
+        <p className="register__footer">
+          Already have an account?
+          <Link className="register__link" to="/">
+            Login
+          </Link>
+        </p>
+
+      </div>
     </div>
-  )
+  );
 }
 
-export default Register
+export default Register;
